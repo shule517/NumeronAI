416b6ffbb2fb226ad957ef7f041a46409c1b632c
shule517 <shule517@gmail.com>
2013-11-15 23:50:35 +0900

    数値チェック処理の追加

---
+++ NumeronAI/NumeronAI/GameMaster.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Collections.Generic;
+namespace NumeronAI
+{
+	public class GameMaster
+	{
+		/// <summary>
+		/// 桁数
+		/// </summary>
+		private const int NumeronDigit = 3;
+
+		/// <summary>
+		/// 判定
+		/// </summary>
+		public JudgeResult Judge(List<int> number1, List<int> number2)
+		{
+			// 数値チェック
+			if ((!CheckNumber(number1)) || (!CheckNumber(number2)))
+			{
+				return new JudgeResult { Eat = -1, Bite = -1 };
+			}
+
+			// EATの判定
+
+
+			// BITEの判定
+
+
+			return new JudgeResult { Bite = 0, Eat = 0 };
+		}
+
+		/// <summary>
+		/// 数値チェック
+		/// </summary>
+		private bool CheckNumber(List<int> number)
+		{
+			// 桁数チェック
+			if (!CheckDigit(number))
+			{
+				return false;
+			}
+
+			// 桁ごとに重複しないこと
+			if ((number[0] == number[1]) ||
+				(number[0] == number[2]) ||
+				(number[1] == number[2]))
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+
+		/// <summary>
+		/// 桁数のチェック
+		/// </summary>
+		private static bool CheckDigit(List<int> number)
+		{
+			return (number.Count == NumeronDigit);
+		}
+	}
+
+	/// <summary>
+	/// 判定結果
+	/// </summary>
+	public class JudgeResult
+	{
+		/// <summary>
+		/// 数字と桁が合っていた場合
+		/// </summary>
+		public int Eat = -1;
+
+		/// <summary>
+		/// 数字は合っているが桁は合っていない場合
+		/// </summary>
+		public int Bite = -1;
+	}
+}
---
+++ NumeronAI/UnitTestNumeron/UnitTestGameMaster.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumeronAI;
+using System.Collections.Generic;
+
+namespace UnitTestNumeron
+{
+	[TestClass]
+	public class UnitTestGameMaster
+	{
+		[TestMethod]
+		public void TestJudge()
+		{
+
+			List<int> number = new List<int>() { 1, 2, 3 };
+
+			// 例外
+			test(new List<int>() { 0, 0, 0 }, new List<int>() { 1, 2, 3 }, new JudgeResult { Eat = -1, Bite = -1 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 0, 0, 0 }, new JudgeResult { Eat = -1, Bite = -1 });
+
+			// 正常
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 1, 2, 3 }, new JudgeResult { Eat = 3, Bite = 0 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 1, 2, 4 }, new JudgeResult { Eat = 2, Bite = 0 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 1, 4, 5 }, new JudgeResult { Eat = 1, Bite = 0 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 3, 1, 2 }, new JudgeResult { Eat = 0, Bite = 3 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 2, 1, 4 }, new JudgeResult { Eat = 0, Bite = 2 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 3, 4, 5 }, new JudgeResult { Eat = 0, Bite = 1 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 4, 5, 6 }, new JudgeResult { Eat = 0, Bite = 0 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 1, 3, 4 }, new JudgeResult { Eat = 1, Bite = 1 });
+			test(new List<int>() { 1, 2, 3 }, new List<int>() { 1, 3, 2 }, new JudgeResult { Eat = 1, Bite = 2 });
+
+			// EAT2はあり得ない -> つまりEAT3
+		}
+
+		private static void test(List<int> number1, List<int> number2, JudgeResult answer)
+		{
+			GameMaster master = new GameMaster();
+			JudgeResult result = master.Judge(number1, number2);
+
+			Assert.AreEqual(result.Eat, answer.Eat);
+			Assert.AreEqual(result.Bite, answer.Bite);
+		}
+	}
+}
